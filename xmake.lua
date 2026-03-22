2c193ce558b236b6314d9da332c742aecc7038fd
chou76904-droid <chou76904@gmail.com>
2026-03-23 00:13:04 +0800

    Add files via upload

--- xmake.lua
+++ xmake.lua
@@ -1,15 +1,5 @@
-set_project("oh-my-robot")
-set_xmakever("3.0.7")
-add_rules("mode.debug", "mode.release")
-add_rules("plugin.compile_commands.autoupdate", {outputdir = os.projectdir()})
+--- @file awlf/xmake.lua
+--- @brief AWLF 构建入口转发
+--- @details 统一入口已迁移至 awlf/build/xmake.lua。
 
-includes("oh-my-robot")
-
-target("robot_project")
-    set_kind("binary")
-    set_filename("robot_project.elf")
-    add_deps("tar_oh_my_robot")
-    set_policy("check.auto_ignore_flags", false)
-    add_rules("oh_my_robot.context", "oh_my_robot.board_assets", "oh_my_robot.image_convert")
-    add_files("oh-my-robot/samples/pal/serial_crc/main.c")	-- 示例文件
-target_end()
+includes("build")
